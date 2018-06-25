83956167b2126ebe95184536bc1b2bbceeab2fbb
DlOMEDES <diomedes.lajara@gmail.com>
2018-06-25 07:57:29 -0400

    updated links with dynamic functions

--- functions.php
+++ functions.php
@@ -1,9 +1,28 @@
 <?php 
 
 function unific_files() {
-  wp_enqueue_style('unific_styles',get_stylesheet_uri());
-}
 
+  // load js scripts('js bundle file'), dependencies, 'version', load before </body>
+  // replace version with mircrotime() to avoid css/js caching in dev phase
+  wp_enqueue_script('unific-js', get_theme_file_uri('js/scripts-bundled.js'), NULL, microtime(), true);
+
+  // load google fonts
+  wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+
+  // add font awesome icons
+  wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+
+  // load stylesheets
+  // dependencies, 'version'
+  wp_enqueue_style('unific_styles',get_stylesheet_uri(), NULL, microtime());
+}
+// ACTIONS
 add_action('wp_enqueue_scripts', 'unific_files');
 
+function unific_features() {
+  add_theme_support('title-tag');
+}
+add_action('after_setup_theme', 'unific_features');
+
 ?>
+
--- page.php
+++ page.php
@@ -1,16 +1,52 @@
 <?php 
 get_header();
 
-echo "<h1>This this is the page file</h1>";
 
 while(have_posts()) {
   the_post(); ?>
 
-  <h2><?php the_title();?></h2>
-  <?php the_content(); ?>
+  <div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/privacy.jpg')?>);"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title"><?php the_title()?></h1>
+      <div class="page-banner__intro">
+        <h4>replace me later</h4>
+      </div>
+    </div>  
+  </div>
+
+  <div class="container container--narrow page-section">
+
+  <?php 
+    // if on parent page(ID 0) dont show metabox 
+    if(wp_get_post_parent_id(get_the_ID())) { ?>
+      <div class="metabox metabox--position-up metabox--with-home-link">
+      <p><a class="metabox__blog-home-link" href="<?php get_permalink(x)?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php get_the_title(x)?></a> <span class="metabox__main"><?php the_title()?></span></p>
+    </div>
+    <?php }
+  
+  ?>
+
+    
+    
+    <!-- <div class="page-links">
+      <h2 class="page-links__title"><a href="#">About Us</a></h2>
+      <ul class="min-list">
+        <li class="current_page_item"><a href="#">Our History</a></li>
+        <li><a href="#">Our Goals</a></li>
+      </ul>
+    </div> -->
+
+    <div class="generic-content">
+      <?php the_content()?>
+    </div>
+
+  </div>
  
 <?php }
 
 
 get_footer();
 ?>
+
+
