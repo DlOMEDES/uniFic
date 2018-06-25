d1d017bb812f7a460c8607d9f2d1b79368d48993
DlOMEDES <diomedes.lajara@gmail.com>
2018-06-25 04:44:14 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+
+echo "<h1>This this is the index file</h1>";
+
+while(have_posts()) {
+  the_post(); ?>
+
+  <h2><a href="<?php the_permalink();?>"><?php the_title();?></h2></a>
+  <?php the_content(); ?>
+  <hr>
+<?php }
+
+?>
